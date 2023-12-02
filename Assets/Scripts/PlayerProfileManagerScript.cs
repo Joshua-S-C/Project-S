47c47ae4871dbd3c242086a509ea329b0c2fae26
Kalib LaClair <kaliblaclair05@gmail.com>
2023-12-02 14:04:47 -0500

    Finished dropdowns

--- Assets/Scripts/PlayerProfileManagerScript.cs
+++ Assets/Scripts/PlayerProfileManagerScript.cs
@@ -1,16 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerProfileManagerScript : MonoBehaviour
 {
+    private static PlayerProfileManagerScript instance;
     private static List<PlayerProfile> profiles = new List<PlayerProfile>();
     private static List<int> newPlayersAddedIndex;
+
+    [Serializable]
+    public struct PlayerDropdowns
+    {
+        public TMP_Dropdown primary;
+        public TMP_Dropdown secondary;
+        public TMP_Dropdown tactical;
+    }
+    public PlayerDropdowns[] playerDropdowns = new PlayerDropdowns[4];
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
         newPlayersAddedIndex = new List<int>();
         if (profiles.Count > 0)
         {
@@ -40,10 +55,14 @@ public class PlayerProfileManagerScript : MonoBehaviour
             newPlayersAddedIndex.Add(input.playerIndex);
             string name = "Player " + (input.playerIndex + 1);
             profiles.Add(new PlayerProfile(input.devices[0],name));
+            PlayerDropdowns currentDropdowns = instance.playerDropdowns[input.playerIndex];
+            currentDropdowns.primary.interactable = true;
+            currentDropdowns.secondary.interactable = true;
+            currentDropdowns.tactical.interactable = true;
         }
-        
-
     }
+
+  
 
     //checks if the profiles array already has a profile with this input device
     private static bool ContainsDevice(InputDevice newDevice)
@@ -77,6 +96,48 @@ public class PlayerProfileManagerScript : MonoBehaviour
     {
         return profiles.Count;
     }
+
+    public void SetPlayerPrimary(int dropdownItemIndex, int playerIndex)
+    {
+        Debug.Log(dropdownItemIndex);
+        if (playerIndex >= profiles.Count) return;
+        PlayerProfile profile = profiles[playerIndex];
+        PrimaryWeapon newWeapon = (PrimaryWeapon)dropdownItemIndex;
+        profile.primary = newWeapon;
+        Debug.Log(profile.primary);
+    }
+    public void SetPlayerSecondary(int dropdownItemIndex, int playerIndex)
+    {
+        Debug.Log(dropdownItemIndex);
+        if (playerIndex >= profiles.Count) return;
+        PlayerProfile profile = profiles[playerIndex];
+        SecondaryWeapon newWeapon = (SecondaryWeapon)dropdownItemIndex;
+        profile.secondary = newWeapon;
+    }
+    public void SetPlayerTactical(int dropdownItemIndex, int playerIndex)
+    {
+        Debug.Log(dropdownItemIndex);
+        if (playerIndex >= profiles.Count) return;
+        PlayerProfile profile = profiles[playerIndex];
+        TacticalWeapon newWeapon = (TacticalWeapon)dropdownItemIndex;
+        profile.tactical = newWeapon;
+    }
+    public void SetPlayer1Primary(int dropdownItemIndex)
+    {
+        SetPlayerPrimary(dropdownItemIndex, 0);
+    }
+    public void SetPlayer2Primary(int dropdownItemIndex)
+    {
+        SetPlayerPrimary(dropdownItemIndex, 1);
+    }
+    public void SetPlayer3Primary(int dropdownItemIndex)
+    {
+        SetPlayerPrimary(dropdownItemIndex, 2);
+    }
+    public void SetPlayer4Primary(int dropdownItemIndex)
+    {
+        SetPlayerPrimary(dropdownItemIndex, 3);
+    }
 }
 
 //contains all the information connecting a player to an input device
@@ -85,6 +146,9 @@ public class PlayerProfile
     private InputDevice device;
     private string playerName;
     int index;
+    public PrimaryWeapon primary = PrimaryWeapon.SemiAutoRifle;
+    public SecondaryWeapon secondary = SecondaryWeapon.Pistol;
+    public TacticalWeapon tactical = TacticalWeapon.Grenade;
 
     public PlayerProfile(InputDevice newDevice,string name)
     {
---
+++ Assets/Scripts/WeaponEnums.cs
@@ -0,0 +1,23 @@
+public enum PrimaryWeapon
+{
+    SemiAutoRifle = 0,
+    AutomaticSMG = 1,
+    BurstRifle = 2,
+    Shotgun = 3,
+    Sword = 4,
+    RPG = 5
+}
+
+public enum SecondaryWeapon
+{
+    Pistol = 0,
+    Knife = 1
+}
+
+public enum TacticalWeapon
+{
+    Grenade = 0,
+    C4 = 1,
+    StunGrenade = 2,
+    ThrowingKnife = 3
+}
